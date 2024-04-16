e77538a24e0ef6812c11a0690ea2db40c101cabe
Andrzej Chmielewski <achmielewski@google.com>
2024-04-16 13:14:01 +0000

    someonelikestrains4

--- chal-mlflow/challenge/start.sh
+++ chal-mlflow/challenge/start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "[mlflow]
-mlflow_tracking_username = username
-mlflow_tracking_password = password" > /.mlflow/credentials
-
-(/usr/local/bin/mlflow server --host 0.0.0.0 --app-name basic-auth) > /dev/null 2>&1 &
+cp /basic_auth.db /home/user
+(/usr/local/bin/mlflow server --host 0.0.0.0 --app-name basic-auth --workers=1) > /dev/null 2>&1 &
 socat - TCP:127.0.0.1:5000,forever 
