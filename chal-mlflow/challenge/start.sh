2011333c1657426425e26d282d8627ca6ff8b44a
AndrzejChmielewski <adek.chmiel@gmail.com>
2024-04-19 13:52:31 +0000

    v0.00010

--- chal-mlflow/challenge/start.sh
+++ chal-mlflow/challenge/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp /basic_auth.db /home/user
-(/usr/local/bin/mlflow server --host 0.0.0.0 --app-name basic-auth --workers=1) > /logs/log 2>&1 &
+(/usr/local/bin/mlflow server --host 0.0.0.0 --dev --app-name basic-auth --workers=1) >> /logs/log 2>&1 &
 socat - TCP:127.0.0.1:5000,forever 
 
 #echo $ROOTFLAG > /flag/flag-root.txt
