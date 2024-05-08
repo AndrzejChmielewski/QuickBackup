f9b69976ef29f3e7636a0d749cbb41f12a5c6b80
AndrzejChmielewski <adek.chmiel@gmail.com>
2024-05-08 14:02:51 +0000

    likebikes2312

--- chal-laravel-latest/challenge/start.sh
+++ chal-laravel-latest/challenge/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-/usr/local/bin/php /var/www/html/laravel/artisan serve --host=0.0.0.0 > /dev/null &
+/usr/local/bin/php /var/www/html/laravel/artisan serve --host=0.0.0.0 &
+/usr/local/bin/php /var/www/html/laravel/artisan /vendor/laravel/pail > /logs/log 2>&1 &
 socat - TCP:127.0.0.1:8000,forever
