8aa561d559cc65758d5bbe1f92e051246cbda4a0
AndrzejChmielewski <adek.chmiel@gmail.com>
2024-06-13 10:53:15 +0000

    drupall

--- chal-drupal-latest/challenge/start.sh
+++ chal-drupal-latest/challenge/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp -r /copy/sites/* /opt/drupal/web/sites/
+
+#sqlite3 /opt/drupal/web/sites/default/files/.ht.sqlite "UPDATE users SET password_hash = '$PASSWORD' WHERE id = 1;"
 apache2-foreground > /dev/null 2>&1 &
 
 # Proxy stdin/stdout to server
