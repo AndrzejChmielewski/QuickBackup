2af5b5923ff09de84405919e724a4c446938e7db
AndrzejChmielewski <adek.chmiel@gmail.com>
2024-07-01 11:40:00 +0000

    drupalALL

--- chal-drupal-latest/challenge/auth-flag.php
+++ chal-drupal-latest/challenge/auth-flag.php
@@ -3,16 +3,8 @@
 use Drupal\Component\Utility\Html;
 use \Drupal\node\Entity\Node;
 
-$flag_path = '/flag/flag-exposed-auth-bypass.txt';
-$flag_contents = file_get_contents($flag_path);
-
-if ($flag_contents) {
-   $flag_contents = Html::escape($flag_contents);
-   $nodes = Node::loadMultiple();
-   foreach($nodes as $node){
-     $node->set('title', $flag_contents);
-     $node->save();
-   }
- }
-
-#drush php-script script2.php
+$nodes = Node::loadMultiple();
+foreach($nodes as $node){
+   $node->set('title', 'abc');
+   $node->save();
+}
--- chal-drupal-latest/challenge/start.sh
+++ chal-drupal-latest/challenge/start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-file="/opt/drupal/web/sites/default/files/A"
-if [ ! -f "$file" ] 
-    then
-    /opt/drupal/vendor/bin/drush php-script /opt/drupal/auth-flag.php
-    touch /opt/drupal/web/sites/default/files/A
-fi
 
-sqlite3 /opt/drupal/web/sites/default/files/.ht.sqlite "UPDATE users_field_data SET pass = '$PASSWORD';"
+/opt/drupal/vendor/bin/drush php-script /opt/drupal/auth-flag.php > /logs/$(date +%s).log 2>&1
+/opt/drupal/vendor/bin/drush user:password admin "$PASSWORD" > /logs/$(date +%s).log 2>&1
 
-apache2-foreground > /dev/null 2>&1 &
+apache2-foreground > /logs/$(date +%s).log 2>&1 &
 
 # Proxy stdin/stdout to server
 socat - TCP:127.0.0.1:8080,forever
