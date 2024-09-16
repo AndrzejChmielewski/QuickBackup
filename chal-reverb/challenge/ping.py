e69083eb879458ba9719170b2bc10a9c45fa2a25
AndrzejChmielewski <adek.chmiel@gmail.com>
2024-09-16 09:12:17 +0000

    yamlupdate

--- chal-reverb/challenge/ping.py
+++ chal-reverb/challenge/ping.py
@@ -1,12 +1,15 @@
 import reverb
 import tensorflow as tf
 
-# Define the client
-client = reverb.Client(f'localhost:8888')
 
-# Check if the client can connect to the server
-print("Checking connection...")
-if client.server_info():
-    print("Connected to the Reverb server!")
-else:
-    print("Failed to connect to the Reverb server.")
+client = reverb.Client(f'localhost:8888')
+print(client.server_info())
+## Define the client
+#client = reverb.Client(f'localhost:8888')
+#
+## Check if the client can connect to the server
+#print("Checking connection...")
+#if client.server_info():
+#    print("Connected to the Reverb server!")
+#else:
+#    print("Failed to connect to the Reverb server.")
